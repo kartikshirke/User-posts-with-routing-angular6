9f5c4d8100e7173ce827a16347a785b26c6b35dc
Kartik_shirke <Kartikdeepakshirke@gmail.com>
2020-07-11 19:37:22 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# Angular 6 Learning 
+
+Angular 6 crash course click <a href="https://coursetro.com/posts/code/154/Angular-6-Tutorial---Learn-Angular-6-in-this-Crash-Course">here</a>
+
+Login and Router <a href="https://medium.com/@ozgurgul/angular-6-login-and-router-tutorial-ac5fc5d3027f">here</a>
+
+## Prerequisites
+
+Node.js and npm are essential to Angular development. 
+    
+<a href="https://docs.npmjs.com/getting-started/installing-node" target="_blank" title="Installing Node.js and updating npm">
+Get it now</a> if it's not already installed on your machine.
+
+
+## Install npm packages
+
+```bash
+npm i
+ng serve -o
+```
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: LoginPageComponent
+  // },
+  {
+    path: '',
+    component: UsersComponent
+  },
+  {
+    path: 'details/:id',
+    component: DetailsComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
